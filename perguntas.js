727d19ad0ab623ce711e22ad1ef9a717c7631f31
BIEL <168236810+gabriel-pinhanelli@users.noreply.github.com>
2024-11-05 17:48:48 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'BIOLOGIA',
+    'O DNA É FORMADO POR 3 ELEMENTOS CRUCIAIS, QUAIS SÃO ELES?',
+    'O DNA é formado por 3 elemento que são as BASES NITROGENADAS, uma PENTOSE e um FOSFATO'
+)
+
+criaCartao(
+    'FILOSOFIA',
+    'O FILÓSOFO THOMAS HOBBES DEFENDIA UMA IDEIA QUE DESTACAVA A IMPORTÂNCIA DAS INTITUIÇÕES E DE NORMAS SOCIAIS, ESTE PENSAMENTO PODE SER REDUZIDO POR UMA FRASE, QUE FRASE É?',
+    'O filósofo defendia uma ideia de que nós eramos inimigos de nós, ou seja, a frase diz "O homem é o lobo do homem" destacando que em estado de perigo não pensamos no outro e sim em nós'
+)
+
+criaCartao(
+    'MATEMATICA',
+    'UMA PARÁBOLA EM UM GRÁFICO, A FUNÇÃO SE CHAMA COMO?',
+    'A função de uma parabola pode ser chamada de função quadrática, mesmo que seja semelhante á um círculo, ela é chamado de quadrática.'
+)
+
+criaCartao(
+    'FISICA',
+    'JORGE COM SUA ESPOSA NO SHOPPING FAZENDO COMPRAS, OBSERVA UMA SILHUETA SEMELHANTE EM SUA FRENTE, PORÉM ELE NÃO EXERGA DE LONGE, MAS AQUELA SILHUETA É DE SUA AMANTE VINDO EM SUA DIREÇÃO, PORÉM ELA NÃO SABE QUE VOCÊ É CASADO, QUAL DOEÇÃO ELE TEM NO OLHO?',
+    'JORGE, apesar de sua falta de carater, ele tem míopia e faz sua esposa de corna'
+)
